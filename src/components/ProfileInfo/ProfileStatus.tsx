f50e090f37af19eca3c773abb4465a78f9186008
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-08-06 21:12:24 +0300

    84.hook useState

--- src/components/ProfileInfo/ProfileStatus.tsx
+++ src/components/ProfileInfo/ProfileStatus.tsx
@@ -1,57 +1,75 @@
-import React, {ChangeEvent} from "react";
+import React, {ChangeEvent, useState} from "react";
 
-type StateType = {
-    status: string
-    editMode: boolean
-}
 type PropsType = {
     status: string
     updateProfileStatus: (status: string) => void
 }
 
-export class ProfileStatus extends React.Component<PropsType, StateType> {
-    state: StateType = {
-        editMode: false,
-        status: this.props.status
+export const ProfileStatus = ({status, updateProfileStatus}: PropsType) => {
+    const [editMode, setEditMode] = useState(false)
+    const [localStatus, setLocalStatus] = useState(status)
+
+    const activateEditMode = () => {
+        setEditMode(true)
     }
 
-    activateEditMode = () => {
-        this.setState({
-            editMode: true
-        })
-    }
-    deactivateEditMode = () => {
-        this.setState({
-            editMode: false,
-        })
-        this.props.updateProfileStatus(this.state.status)
-    }
-    onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        updateProfileStatus(localStatus)
     }
 
-    // componentDidUpdate(prevProps: Readonly<PropsType>, prevState: Readonly<{}>, snapshot?: any) {
-    //     if (prevProps.status !== this.props.status) {
-    //         this.setState({status: this.props.status})
-    //     }
-    // }
-
-    componentDidUpdate(prevProps: PropsType, prevState: StateType) {
-        if (prevProps.status !== this.props.status) {
-            this.setState({status: this.props.status})
-        }
+    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
+        setLocalStatus(e.currentTarget.value)
     }
 
-    render() {
-        return this.state.editMode
-            ? <input value={this.state.status}
-                     onBlur={this.deactivateEditMode} autoFocus
-                     onChange={this.onChangeStatus}
-            />
-            : <span onDoubleClick={this.activateEditMode}>
-                {this.props.status}
+    return editMode
+        ? <input value={localStatus}
+                 onBlur={deactivateEditMode} autoFocus
+                 onChange={onChangeStatus}
+        />
+        : <span onDoubleClick={activateEditMode}>
+                {status}
             </span>
-    }
+
 }
+
+
+
+
+
+
+//=====================================
+
+// state: StateType = {
+//     editMode: false,
+//     status: this.props.status
+// }
+//
+// activateEditMode = () => {
+//     this.setState({
+//         editMode: true
+//     })
+// }
+// deactivateEditMode = () => {
+//     this.setState({
+//         editMode: false,
+//     })
+//     this.props.updateProfileStatus(this.state.status)
+// }
+// onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
+//     this.setState({
+//         status: e.currentTarget.value
+//     })
+// }
+//
+// // componentDidUpdate(prevProps: Readonly<PropsType>, prevState: Readonly<{}>, snapshot?: any) {
+// //     if (prevProps.status !== this.props.status) {
+// //         this.setState({status: this.props.status})
+// //     }
+// // }
+//
+// componentDidUpdate(prevProps: PropsType, prevState: StateType) {
+//     if (prevProps.status !== this.props.status) {
+//         this.setState({status: this.props.status})
+//     }
+// }
